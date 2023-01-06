580d51dd2e56d514ee0b7cf46d9e3c66377ce007
Killian356 <Killian7195@gmail.com>
2023-01-06 13:38:09 -0600

    fixing typo

--- variables1.c
+++ variables1.c
@@ -4,6 +4,6 @@ int main()
     //Variables
     //Example 1
     int playerPoints = 95;
-    //playerPoints is an int-type variable in this case. The variable is givent the integer value of 95.
+    //playerPoints is an int-type variable in this case. The variable is given the integer value of 95.
     return 0;
 }
