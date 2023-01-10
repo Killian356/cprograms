d8e0da1e3f34d2be27c1a6cc2130012d84e39518
Killian356 <Killian7195@gmail.com>
2023-01-10 11:21:02 -0600

    added int main() to chartype1 and chartype2 - Added chartype3 c program

--- chartype1.c
+++ chartype1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 //Defining a variable of a type character - Proceding the variable name with a char keyword.
+int main()
 {
     char ch;
     char key, flag;
---
+++ chartype3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// Char type as an integer
+// Initializing or assiging an integer to a char variable
+// (not the best practice or recomended as the code might not be portable)
+int main()
+{
+    ch = 66;
+}
