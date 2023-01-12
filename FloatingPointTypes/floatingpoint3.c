a4614fbe51d438cc53dafca31c53c2adb3d2e667
Killian356 <Killian7195@gmail.com>
2023-01-12 11:31:10 -0600

    adding multipel floating point addition program, floatingpoint3 program.

---
+++ FloatingPointTypes/floatingpoint3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//Adding  Multiple defined floating points
+int main()
+{
+    float x = 0.1,
+    y = 0.1,
+    z = 0.1;
+
+    float total = x + y + z;
+
+    printf("The sum is %.20f\n", total);
+    return 0;
+}
