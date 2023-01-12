e860a1533907200fa90ea40c09792a3a3e6be2dc
Killian356 <Killian7195@gmail.com>
2023-01-12 15:03:32 -0600

    Added strings protocol initilization

---
+++ Strings/strings3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//How strings shoudl be initialized
+int main()
+{
+    char c1[] = "abcd";
+    char c2[] = "abcd";
+    char c3[] = {'a', 'b', 'c', 'd', '\0'};
+    char c4[] = {'a', 'b', 'c', 'd', '\0'};
+}
