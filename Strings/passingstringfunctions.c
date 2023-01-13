adb25ce5d45e84962f3c7c440a4290dd63e740f7
Killian356 <Killian7195@gmail.com>
2023-01-13 11:46:38 -0600

    added program to demonstrate passing strings to functions

---
+++ Strings/passingstringfunctions.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// Passing strings to functions
+void displayString(char str[]);
+int main()
+{
+    char str[50];
+    printf("Enter string: ");
+    fgets(str, sizeof(str), stdin);
+    displayString(str); //Passing string into a function.
+    return 0;
+}
