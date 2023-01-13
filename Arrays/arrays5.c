3816e4d10a0de548ecc67d8593d73e7f9941c3b9
Killian356 <Killian7195@gmail.com>
2023-01-13 13:59:50 -0600

    adding modifiable array elements program

---
+++ Arrays/arrays5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//Modifing Value of array elements
+int main()
+{
+    int mark[5] = {18, 11, 9, 16, 8};
+    //Set the third element's value to -1
+    mark[2] = -1;
+    //Set the fifth element's value to 0
+    mark[4] = 0;
+}
