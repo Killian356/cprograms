8d3234216bbecb25186ef0bd3fdee4d7d0e5960e
Killian356 <Killian7195@gmail.com>
2023-01-13 13:53:56 -0600

    adding example of undefined array

---
+++ Arrays/arrays4.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+//Example of un defined array, the compiler will know how many based on how many single numbers are contained within it.
+int main()
+{
+    int mark[] = {18, 11, 9, 16, 8};
+}
